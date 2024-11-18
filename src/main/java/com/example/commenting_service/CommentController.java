1693dc456a1e07d7a5217f9477daad651883e78c
David Adediji <david.adediji@sil.mu>
2024-11-18 19:30:31 +0400

    Implement pagination for fetch all

--- src/main/java/com/example/commenting_service/CommentController.java
+++ src/main/java/com/example/commenting_service/CommentController.java
@@ -1,12 +1,13 @@
 package com.example.commenting_service;
 
-
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -16,10 +17,12 @@ public class CommentController {
     private final CommentService commentService;
 
     private final CommentMapper commentMapper;
+    private final CommentRepository commentRepository;
 
-    public CommentController(CommentService commentService, CommentMapper commentMapper) {
+    public CommentController(CommentService commentService, CommentMapper commentMapper, CommentRepository commentRepository) {
         this.commentService = commentService;
         this.commentMapper = commentMapper;
+        this.commentRepository = commentRepository;
     }
 
     @GetMapping("/{id}")
@@ -30,19 +33,26 @@ public class CommentController {
 
     @PostMapping
     public ResponseEntity<Comment> createComment(@Valid @RequestBody CommentDTO commentDTO) {
-       try{
-        Comment comment = commentMapper.commentDTOToComment(commentDTO);
-        Comment savedComment = commentService.createComment(comment);
-        return ResponseEntity.ok(savedComment);}
-       catch(Exception e){
-           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-       }
+        try {
+            Comment comment = commentMapper.commentDTOToComment(commentDTO);
+            Comment savedComment = commentService.createComment(comment);
+            return ResponseEntity.ok(savedComment);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
     @GetMapping
-    public ResponseEntity<List<Comment>> getAllComments() {
-        Optional<List<Comment>> comments = commentService.getAllComments();
-        return comments.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<Page<Comment>> getAllComments(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "createdAt") String sortBy,
+            @RequestParam(defaultValue = "desc") String order
+    ) {
+        Sort sort = order.equalsIgnoreCase("desc")
+                ? Sort.by(sortBy).descending()
+                : Sort.by(sortBy).ascending();
+        return ResponseEntity.ok(commentRepository.findAll(PageRequest.of(page, size, sort)));
     }
 
     @PostMapping("/{id}")
@@ -56,7 +66,7 @@ public class CommentController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Comment> updateComment(@PathVariable String id, @RequestBody CommentDTO commentDTO){
+    public ResponseEntity<Comment> updateComment(@PathVariable String id, @RequestBody CommentDTO commentDTO) {
         Comment comment = commentMapper.commentDTOToComment(commentDTO);
         Comment updatedComment = commentService.updateComment(id, comment);
         return ResponseEntity.ok(updatedComment);
--- src/main/java/com/example/commenting_service/CommentRepository.java
+++ src/main/java/com/example/commenting_service/CommentRepository.java
@@ -1,8 +1,13 @@
 package com.example.commenting_service;
 
+import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CommentRepository extends MongoRepository<Comment, String> {
 
-
+    @Override
+    @NonNull
+    Page<Comment> findAll(@NonNull Pageable pageable);
 }
